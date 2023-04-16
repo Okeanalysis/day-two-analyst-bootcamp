8f1facc1503af981a55e5c6a632b70aa3df0bae2
emmanuel214 <77278849+emmanuel214@users.noreply.github.com>
2023-04-16 20:02:57 +0100

    Add files via upload
    This code shows what you can do with SELECT statement in SQL

---
+++ day 2.sql
@@ -0,0 +1,30 @@
+--SELECT *
+--FROM EmployeeDemographics
+
+--SELECT FirstName, LastName
+--FROM EmployeeDemographics
+
+
+--SELECT TOP 5*
+--FROM EmployeeDemographics
+
+--SELECT DISTINCT(EmployeeID)
+--FROM EmployeeDemographics
+
+--SELECT COUNT(LastName)
+--FROM EmployeeDemographics
+
+--SELECT COUNT(LastName) AS LastNamecount
+--FROM EmployeeDemographics
+
+--SELECT MAX(Salary)
+--FROM EmployeeSalary
+
+--SELECT MIN(Salary)
+--FROM EmployeeSalary
+
+--SELECT AVG(Salary)
+--FROM EmployeeSalary
+
+--SELECT*
+--FROM SQLFIRST.dbo.EmployeeSalary
